1e902d92c1f607cbc6569b16aed1d32b1f8554a7
Naranmã Sousa Santos <naranma.sousa@tre-to.jus.br>
2019-08-07 14:14:29 -0300

    Protegendo as rotas com autenticação

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,13 @@ Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 
-
-Route::get('/snippet/index', 'SnippetController@index')->where('id', '[0-9]+')->name('snippet.index');
-Route::get('/snippet/{id}/show', 'SnippetController@show')->where('id', '[0-9]+')->name('snippet.show');
-Route::get('/snippet/create', 'SnippetController@create')->name('snippet.create');
-Route::get('/snippet/{id}/edit', 'SnippetController@edit')->where('id', '[0-9]+')->name('snippet.edit');
-Route::post('/snippet', 'SnippetController@store')->name('snippet.store');
-Route::post('/snippet/{id}', 'SnippetController@update')->where('id', '[0-9]+')->name('snippet.update');
-Route::delete('/delete', 'SnippetController@destroy')->name('snippet.destroy');
-
+Route::middleware('auth')->group(function () {
+    Route::get('/snippet/index', 'SnippetController@index')->where('id', '[0-9]+')->name('snippet.index');
+    Route::get('/snippet/{id}/show', 'SnippetController@show')->where('id', '[0-9]+')->name('snippet.show');
+    Route::get('/snippet/create', 'SnippetController@create')->name('snippet.create');
+    Route::get('/snippet/{id}/edit', 'SnippetController@edit')->where('id', '[0-9]+')->name('snippet.edit');
+    Route::post('/snippet', 'SnippetController@store')->name('snippet.store');
+    Route::post('/snippet/{id}', 'SnippetController@update')->where('id', '[0-9]+')->name('snippet.update');
+    Route::delete('/delete', 'SnippetController@destroy')->name('snippet.destroy');
+});
 
